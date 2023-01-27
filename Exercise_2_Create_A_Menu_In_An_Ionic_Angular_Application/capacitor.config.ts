ff267edb5700ecc66b4fb9ff861b44660679db76
yapeaud <yapoabed@gmail.com>
2023-01-27 17:44:59 +0000

    w5d1g

---
+++ Exercise_2_Create_A_Menu_In_An_Ionic_Angular_Application/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Exercise_2_Create_A_Menu_In_An_Ionic_Angular_Application',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
---
+++ Exercise_3_Add_An_Image_To_Your_Ionic_And_Angular_Application/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Exercise_3_Add_An_Image_To_Your_Ionic_And_Angular_Application',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
